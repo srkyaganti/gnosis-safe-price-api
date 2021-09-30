f3a7178af19fc4fd2f761a03539f2b9b72d2c83a
Srikar Yaganti <srikar.yaganti@cred.club>
2021-09-30 15:37:51 +0530

    adds docker-compose, integrates cache to price fetch api

---
+++ redis/index.js
@@ -0,0 +1,13 @@
+const redis = require('redis');
+const client = redis.createClient(process.env.REDIS_URL);
+
+const { promisify } = require("bluebird");
+Promise.promisify
+
+module.exports = {
+    ...client,
+    getAsync: promisify(client.get).bind(client),
+    setAsync: promisify(client.set).bind(client),
+    keysAsync: promisify(client.keys).bind(client),
+    existsAsync: promisify(client.exists).bind(client),
+  };
--- routes/usd-price.js
+++ routes/usd-price.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
+
 const axios = require('axios').default;
+
+const { setAsync, getAsync, keysAsync, existsAsync } = require('../redis');
+
+const redis = require('redis');
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
+const Promise = require("bluebird");
+Promise.promisify
+
+
+const _ = require('lodash');
 
 const GNOSIS_SAFE_BASE_URL = 'https://safe-transaction.gnosis.io/api/v1/';
 
@@ -8,17 +20,52 @@ const fetchPrices = async (addresses) => {
   const validatedAddresses = addresses || [];
 
   const promises = validatedAddresses
-  .map(e => e.trim())
-  .map(address => {
-    axios.get(`${GNOSIS_SAFE_BASE_URL}safes/${address}/balances/usd/`)
-  });
+    .map(e => e.trim())
+    .map(address =>
+      axios.get(`${GNOSIS_SAFE_BASE_URL}safes/${address}/balances/usd/`)
+        .then(response => ({
+          address,
+          success: true,
+          data: response.data
+        }))
+        .catch(error => ({
+          address,
+          success: false,
+          error
+        }))
+    );
 
   return await Promise.all(promises);
-} 
+}
 
 /* Fetch usd prices for given addresses. */
 router.post('/', async (req, res, next) => {
-  res.json({ addresses: await fetchPrices(req.body.addresses) });
+  const addresses = req.body.addresses || [];
+
+  const validatedAddresses = addresses.map(address => address.trim()).filter(e => e);
+
+  if (validatedAddresses.length < 1 && validatedAddresses.length > 100) {
+    res.status(404).send('addresses size should be b/w 1 & 100');
+  }
+
+  const cachedAddresses = validatedAddresses.filter(address => redisClient.exists(address));
+  const addressesToBeFetched = _.difference(validatedAddresses, cachedAddresses);
+
+  const prices = await fetchPrices(addressesToBeFetched);
+
+  const redisSetPromises = prices.filter(price => price.success).map(({ address, data }) => setAsync(address, JSON.stringify(data)));
+
+  await Promise.all(redisSetPromises);
+
+  const redisGetPromises = validatedAddresses.map(async address => new Promise((resolve, reject) => {
+    getAsync(address)
+      .then(response => resolve({ address, data: JSON.parse(response) }))
+      .catch(error => reject({ address, error }));
+  }));
+
+  const fetchedRedisData = await Promise.all(redisGetPromises);
+
+  res.send(fetchedRedisData);
 });
 
 module.exports = router;
